c72e055229a78fd2622f9d16c9a71de38a9652a1
pronoy656 <pronoypaul56@gmail.com>
2024-05-13 20:50:35 +0600

    authorized token added successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require ('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors({
   credentials: true
 }))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -30,6 +32,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+// middlewares
+const logger = (req,res,next) =>{
+  console.log('log info',req.method, req.url);
+  next();
+}
+
+const verifyTokens = (req,res,next) =>{
+  const token = req?.cookies?.token
+  // console.log('token in the middleware', token)
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized access'})
+
+  }
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN,(err,decoded) =>{
+    if(err){
+     return res.status(401).send({message: 'unauthorized Access'})
+    }
+    req.user = decoded;
+    next();
+  })
+
+}
+
 
 async function run() {
   try {
@@ -54,6 +80,13 @@ async function run() {
   })
 
 
+  // log out api
+  app.post('/logout', async(req,res) =>{
+    const user = req.body
+    console.log('log out user', user )
+    res.clearCookie('token', {maxAge: 0}).send({success:true})
+  })
+
 // get add job 
 app.get('/allJobs', async(req,res) =>{
     const courser = jobCollection.find();
@@ -63,7 +96,7 @@ app.get('/allJobs', async(req,res) =>{
 
 
 // add job post
-app.post('/allJobs', async(req,res) =>{
+app.post('/allJobs', logger, async(req,res) =>{
     const newJob = req.body
     console.log(newJob)
     const result = await jobCollection.insertOne(newJob)
@@ -80,8 +113,12 @@ app.get('/allJobs/:id', async (req,res) =>{
 })
 
 // email based data find
-app.get('/myJobs', async (req,res) =>{
+app.get('/myJobs', logger, verifyTokens, async (req,res) =>{
     console.log(req.query.email)
+    console.log('token owner',req.user)
+    if(req.user.email !== req.query.email){
+      return res.status(403).send({message: 'forbidden access'})
+    }
     let query = {}
     if(req.query?.email){
        query = {email: req.query.email}
